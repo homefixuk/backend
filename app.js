b92c6886c948cf5f40e62f24d92c8716646f28a7
niravshah <nirav.shah83@gmail.com>
2016-06-27 22:34:18 +0100

    Docker Changes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var env = process.env.NODE_ENV || 'dev'
 var config = require('./config')[env]
 
-console.log("ENV:",env,congif);
-console.log("ENV:",env,congif);
+console.log("ENV:",env,config);
 
 var express = require('express');
 var app = express();
