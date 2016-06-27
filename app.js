649d3c0ae9fb287d4808c38136a4c1b0368c0d61
niravshah <nirav.shah83@gmail.com>
2016-06-27 15:09:31 +0100

    Tradesman API

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 var flash = require('connect-flash');
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('view cache', false);
-swig.setDefaults({ cache: false });
+swig.setDefaults({cache: false});
 
 var passport = require('passport');
 var expressSession = require('express-session');
@@ -44,39 +44,42 @@ app.use(logger('dev'));
 var routes = require('./routes/index')(passport);
 var tradesman = require('./routes/tradesman/index')(passport);
 app.use('/', routes);
-app.use('/tradesman',tradesman);
+app.use('/tradesman', tradesman);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'dev') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if (env === 'dev') {
+    app.use(function (err, req, res, next) {
+        console.log("Dev Error Handler", err);
+        res.status(err.status || 500);
+        res.status(500).json({
+            message: err.message,
+            error: err
+        });
     });
-  });
-}
-
+} else {
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+    app.use(function (err, req, res, next) {
+        console.log("Prod Error Handler", err);
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
 //module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,56 +2,66 @@ var express = require('express');
 var router = express.Router();
 
 var isAuthenticated = function (req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  res.redirect('/');
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
 }
 
-module.exports = function(passport){
+module.exports = function (passport) {
 
-  /* GET login page. */
-  router.get('/', function(req, res) {
-    res.render('index', { message: req.flash('message') });
-  });
+    /* GET login page. */
+    router.get('/', function (req, res) {
+        res.render('index', {message: req.flash('message')});
+    });
 
-  /* Handle Login POST */
-  router.post('/login', passport.authenticate('login', {
-    successRedirect: '/home',
-    failureRedirect: '/',
-    failureFlash : true
-  }));
+    /* Handle Login POST */
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/home',
+        failureRedirect: '/',
+        failureFlash: true
+    }));
 
-  /* GET Registration Page */
-  router.get('/signup', function(req, res){
-    res.render('register',{message: req.flash('message')});
-  });
+    /* GET Registration Page */
+    router.get('/signup', function (req, res) {
+        res.render('register', {message: req.flash('message')});
+    });
 
-  /* Handle Registration POST */
-  router.post('/signup', passport.authenticate('signup', {
-    successRedirect: '/home',
-    failureRedirect: '/signup',
-    failureFlash : true
-  }));
+    /* Handle Registration POST */
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/home',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
 
-  /* GET Home Page */
-  router.get('/home', isAuthenticated, function(req, res){
-    res.render('home', { user: req.user });
-  });
+    /* GET Home Page */
+    router.get('/home', isAuthenticated, function (req, res) {
+        res.render('home', {user: req.user});
+    });
 
-  router.get('/client/unauthorized', function (req, res) {
-    res.json({message: "Unauthorized Client"})
-  });
+    router.get('/client/unauthorized', function (req, res) {
+        res.json({message: "Unauthorized Client"})
+    });
 
-  router.post('/clients', function (req, res) {
-    console.log(req.body.key);
-    
-  });
+    router.post('/clients', function (req, res) {
+        console.log(req.body.key);
+
+        var client = new Client();
+        client.apiKey = req.body.key;
+        client.description = req.body.description;
+        client.save(function (err) {
+            console.log(err);
+            if (err) {
+                res.json({message: "Could not create new client", error: err});
+            }
+            res.json({message: "New API Key created"});
+        })
+    });
 
 
-  router.get('/signout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
+    router.get('/signout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
-  return router;
+    return router;
 }
