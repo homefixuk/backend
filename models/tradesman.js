b2e34cfeb299bf5d5d2b94775b6b2b1a1ace6baf
niravshah <nirav.shah83@gmail.com>
2016-07-05 21:12:56 +0100

    Tests

--- models/tradesman.js
+++ models/tradesman.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Tradesman',{
-    user: {type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    user: {type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
     type:String,
     rating:String,
     numberOfReviews:String,
