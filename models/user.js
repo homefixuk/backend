bb9890bef68169bd92511e57d0531178cab4aeda
shahn2 <nirav.shah@capita.co.uk>
2016-09-16 14:25:04 +0100

    Updates

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('User', {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, validate: emailValidator },
-    password: { type: String, required: true, validate: passwordValidator },
+    email: { type: String, required: true},
+    password: { type: String, required: true},
     role: { type: String, enum: ['TRADE', 'CUST'], default: 'CUST' },
     billingAddressLine1: { type: String, default: '' },
     billingAddressLine2: { type: String, default: '' },
