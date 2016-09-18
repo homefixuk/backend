ee61f7c0a84f8a42f686c7a591e18af28771f3c4
niravshah <nirav.shah83@gmail.com>
2016-09-18 12:02:22 +0100

    made service not required while creating a timeslot

--- models/timeslot.js
+++ models/timeslot.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 module.exports = mongoose.model('Timeslot', {
     tradesman: {type: mongoose.Schema.Types.ObjectId, ref: 'Tradesman', require: true},
-    service: {type: mongoose.Schema.Types.ObjectId, ref: 'Service', require: true},
+    service: {type: mongoose.Schema.Types.ObjectId, ref: 'Service'},
     type: {type: String, enum: ['availability', 'break', 'service', 'own_job'], default: 'availability'},
     start: { type : Number, required:true },
     end: { type : Number, required:true },
