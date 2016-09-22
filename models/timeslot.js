14fe1dada9894b17d911673d5ff703224926a21a
shahn2 <nirav.shah@capita.co.uk>
2016-09-22 11:44:18 +0100

    Slot Length

--- models/timeslot.js
+++ models/timeslot.js
@@ -3,8 +3,8 @@ module.exports = mongoose.model('Timeslot', {
     tradesman: {type: mongoose.Schema.Types.ObjectId, ref: 'Tradesman', require: true},
     service: {type: mongoose.Schema.Types.ObjectId, ref: 'Service'},
     type: {type: String, enum: ['availability', 'break', 'service', 'own_job'], default: 'availability'},
-    start: { type : Number, required:true },
-    end: { type : Number, required:true },
-    slotLength: {type: Number, required:true},
+    start: { type : Number, required:true, default:0 },
+    end: { type : Number, required:true, default:0 },
+    slotLength: {type: Number},
     canBeSplit:{ type : Boolean, default:false }
 });
--- routes/timeslots.js
+++ routes/timeslots.js
@@ -4,21 +4,21 @@ var Tradesman = require('../models/tradesman');
 var Timeslot = require('../models/timeslot');
 var Service = require('../models/service');
 
-router.get('/tradesman/timeslots', function(req, res, next) {
+router.get('/tradesman/timeslots', function (req, res, next) {
     Tradesman.findOne({
         user: req.user
-    }).exec(function(err, tradesman) {
-        if(err) {
+    }).exec(function (err, tradesman) {
+        if (err) {
             var err = new Error('Error encountered while requesting the Tradesman');
             err.status = 500;
             next(err);
         } else {
-            if(tradesman) {
+            if (tradesman) {
 
                 Timeslot.find({
                     tradesman: tradesman
-                }).populate('tradesman').exec(function(err, timeslots) {
-                    if(err) {
+                }).populate('tradesman').exec(function (err, timeslots) {
+                    if (err) {
                         var err = new Error('Timeslots could not be found for this Tradesman');
                         err.status = 500;
                         next(err);
@@ -36,70 +36,70 @@ router.get('/tradesman/timeslots', function(req, res, next) {
     });
 });
 
-router.post('/tradesman/timeslot', function(req, res, next) {
+router.post('/tradesman/timeslot', function (req, res, next) {
     Tradesman.findOne({
         user: req.user
-    }).exec(function(err, tradesman) {
-        if(err) {
+    }).exec(function (err, tradesman) {
+        if (err) {
             var err = new Error('Error encountered while requesting the Tradesman');
             err.err = err;
             err.status = 500;
             next(err);
         } else {
-            if(tradesman) {
-                if(req.query.service){
-                    Service.findOne({_id:req.query.service}).exec(function(err,service){
-                        if(err){
+            if (tradesman) {
+                if (req.query.service) {
+                    Service.findOne({ _id: req.query.service }).exec(function (err, service) {
+                        if (err) {
                             var nErr = new Error('Error encountered while getting Service for Timeslot');
                             nErr.err = err;
                             nErr.status = 500;
                             next(nErr);
-                        }else{
+                        } else {
                             var tslot = new Timeslot();
                             tslot.tradesman = tradesman;
                             tslot.service = service;
-                            if(req.query.end){
+                            if (req.query.end) {
                                 tslot.end = req.query.end;
                             }
-                            if(req.query.start){
+                            if (req.query.start) {
                                 tslot.start = req.query.start;
                             }
-                            if(req.query.slotLength){
-                                tslot.slotLength = req.query.slotLength;
-                            }
-                            tslot.save(function(err,tslot){
-                                if(err){
+
+                            tslot.slotLength = tslot.end - tslot.start;
+
+                            tslot.save(function (err, tslot) {
+                                if (err) {
                                     var nErr = new Error('Error saving Timeslot');
                                     nErr.err = err;
                                     nErr.status = 500;
                                     next(nErr);
-                                }else{
-                                    res.json({success: true, message: 'New Timeslot Created', timeslot: tslot._id});
+                                } else {
+                                    res.json({ success: true, message: 'New Timeslot Created', timeslot: tslot._id });
                                 }
                             });
                         }
                     });
 
-                }else{
+                } else {
                     var tslot = new Timeslot();
                     tslot.tradesman = tradesman;
-                    if(req.query.end){
+                    if (req.query.end) {
                         tslot.end = req.query.end;
                     }
-                    if(req.query.start){
+                    if (req.query.start) {
                         tslot.start = req.query.start;
                     }
-                    if(req.query.slotLength){
-                        tslot.slotLength = req.query.slotLength;
-                    }
-                    tslot.save(function(err,tslot){
-                        if(err){
+
+                    tslot.slotLength = tslot.end - tslot.start;
+
+                    tslot.save(function (err, tslot) {
+                        if (err) {
                             var nErr = new Error('Error saving Timeslot');
                             nErr.err = err;
                             nErr.status = 500;
                             next(nErr);
-                        }else{
-                            res.json({success: true, message: 'New Timeslot Created', timeslot: tslot._id});
+                        } else {
+                            res.json({ success: true, message: 'New Timeslot Created', timeslot: tslot._id });
                         }
                     });
                 }
@@ -113,32 +113,31 @@ router.post('/tradesman/timeslot', function(req, res, next) {
     });
 });
 
-router.patch('/tradesman/timeslot/:id', function(req, res, next) {
-    Timeslot.findOneAndUpdate({_id:req.params.id},req.query, function(err,tslot){
-        if(err){
+router.patch('/tradesman/timeslot/:id', function (req, res, next) {
+    Timeslot.findOneAndUpdate({ _id: req.params.id }, req.query, function (err, tslot) {
+        if (err) {
             var newErr = new Error('Error encountered while updating the Timeslot');
             newErr.error = err;
             newErr.status = 500;
             next(newErr);
-        }else{
-            res.json({success: true, message: 'Timeslot Updated', timeslot: tslot._id});
+        } else {
+            res.json({ success: true, message: 'Timeslot Updated', timeslot: tslot._id });
         }
     });
 });
 
-router.delete('/tradesman/timeslot/:id', function(req, res, next) {
+router.delete('/tradesman/timeslot/:id', function (req, res, next) {
 
-    Timeslot.find({_id:req.params.id}).remove().exec(function(err){
-        if(err){
+    Timeslot.find({ _id: req.params.id }).remove().exec(function (err) {
+        if (err) {
             var newErr = new Error('Error deleting Timeslot');
             newErr.error = err;
             newErr.status = 500;
             next(newErr);
-        }else{
-            res.json({success: true, message: 'Timeslot Deleted'});
+        } else {
+            res.json({ success: true, message: 'Timeslot Deleted' });
         }
     });
 });
-
 
 module.exports = router;
